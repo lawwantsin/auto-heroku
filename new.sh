2063a2ca710dbe0590990047f9f63cd0456e15f2
Lawrence Whiteside <law@cinemasetfree.com>
2015-08-14 13:31:23 -0700

    bundle install issue

--- new.sh
+++ new.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 heroku apps:create $1
-echo "gem 'pg'" >> Gemfile
+if ! grep -q "gem 'pg'" "Gemfile";
+  then
+  echo "gem 'pg'" >> Gemfile
+  bundle install
+fi
+git add .
+git commit -m "added bg to gemfile"
 git push heroku master
 heroku run rake db:migrate
 heroku open
